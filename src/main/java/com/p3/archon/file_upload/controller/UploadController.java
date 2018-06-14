2a1cadb8ab4795794204e2089f10dd9384c4a68c
omjigupta <omji.mcs.du.2014@gmail.com>
2018-06-14 09:49:58 +0530

    added optional parameter & storing file in random created folder

--- src/main/java/com/p3/archon/file_upload/controller/UploadController.java
+++ src/main/java/com/p3/archon/file_upload/controller/UploadController.java
@@ -1,6 +1,7 @@
 package com.p3.archon.file_upload.controller;
 
 import com.p3.archon.common.beans.ApplicationResponse;
+import lombok.NonNull;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.util.StringUtils;
@@ -14,6 +15,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
+import java.util.UUID;
 import java.util.stream.Collectors;
 
 @RestController
@@ -23,63 +25,66 @@ public class UploadController {
   private final Logger logger = LoggerFactory.getLogger(UploadController.class);
 
   /** Save the uploaded file to this folder */
-  private static String UPLOADED_FOLDER;
+  private static String UPLOADED_FOLDER; // ="/Users/omjigupta/Documents/testing/testing";
 
   @GetMapping
   public ApplicationResponse index() {
-    String currentDirectory;
-    //currentDirectory = System.getProperty("user.dir");
-    File file = new File(".");
-    currentDirectory = file.getAbsolutePath();
-    System.out.println("Current working directory : "+currentDirectory);
-    return ApplicationResponse.success("upload_file");
+    return ApplicationResponse.success("Application is running!!");
   }
-
 
   @PostMapping("/multi")
   public ApplicationResponse uploadFileMulti(
-          @RequestParam(value = "name", required=false) String name,
-          @RequestParam("files") MultipartFile[] uploadfiles) {
+      @RequestParam(value = "name", required = false) String name,
+      @RequestParam("files") MultipartFile[] uploadfiles) {
 
     logger.debug("Multiple file upload!");
 
     // Get file name
-    String uploadedFileName = Arrays.stream(uploadfiles).map(x -> x.getOriginalFilename())
-            .filter(x -> !StringUtils.isEmpty(x)).collect(Collectors.joining(" , "));
+    String uploadedFileName =
+        Arrays.stream(uploadfiles)
+            .map(x -> x.getOriginalFilename())
+            .filter(x -> !StringUtils.isEmpty(x))
+            .collect(Collectors.joining(" , "));
 
     if (StringUtils.isEmpty(uploadedFileName)) {
-      return ApplicationResponse.success("please select a file!");
+      return ApplicationResponse.success("Please select a file!");
     }
 
+    UUID id;
+    if (name == null) {
+      id = UUID.randomUUID();
+      name = id.toString().substring(0, 8);
+    }
     try {
-      saveUploadedFiles(Arrays.asList(uploadfiles));
+      saveUploadedFiles(Arrays.asList(uploadfiles), name);
     } catch (IOException e) {
       return ApplicationResponse.failure(e.getMessage());
     }
 
     return ApplicationResponse.success("Successfully uploaded!");
-
   }
 
   /** Save the uploaded file(s) */
-  private void saveUploadedFiles(List<MultipartFile> files) throws IOException {
+  private void saveUploadedFiles(List<MultipartFile> files, String name) throws IOException {
 
     logger.debug("Multiple file upload! With UploadModel");
 
-    String currentDirectory;
-    currentDirectory = System.getProperty("user.dir");
-    UPLOADED_FOLDER = currentDirectory;
+    String currentDirectory = System.getProperty("user.dir");
+    new File(currentDirectory + File.separator + name).mkdir();
+    UPLOADED_FOLDER = currentDirectory + File.separator + name + File.separator;
 
     for (MultipartFile file : files) {
-
       if (file.isEmpty()) {
         continue;
       }
 
-      byte[] bytes = file.getBytes();
-      Path path = Paths.get(file.getOriginalFilename());
-      //System.out.println(path);
-      Files.write(path, bytes);
+      try {
+        byte[] bytes = file.getBytes();
+        Path path = Paths.get(UPLOADED_FOLDER + file.getOriginalFilename());
+        Files.write(path, bytes);
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
     }
   }
 }
