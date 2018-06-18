20dfcb244db7264d9048d340f0ffcfb9b860cc19
svudya <vudhyasaideepak@gmail.com>
2018-06-18 18:48:44 +0530

    added code to create tree jsons

--- src/main/java/com/p3/archon/file_upload/XMLFileReader.java
+++ src/main/java/com/p3/archon/file_upload/XMLFileReader.java
@@ -8,19 +8,20 @@ import java.util.*;
 import org.dom4j.*;
 import org.dom4j.io.SAXReader;
 import org.dom4j.xpath.DefaultXPath;
+import org.json.simple.JSONObject;
 
 /**
  * @author saideepak
  *
  */
 public class XMLFileReader {
-	
+
 	public static void main(String[] args) {
 		XMLFileReader reader = new XMLFileReader();
 		reader.readXml("/Users/saideepak/Projects/JPMC/JPMC_-_XML_Files/CFW_DeceasedCaseHistory1.xsd");
 	}
 
-	public void readXml(String xml) {
+	public JSONObject readXml(String xml) {
 		try {
 			File inputFile = new File(xml);
 			SAXReader reader = new SAXReader();
@@ -30,18 +31,26 @@ public class XMLFileReader {
 			System.out.println(rootElement.toString());
 			String namespace = rootElement.getNamespaceURI();
 			XPath xpath = new DefaultXPath("/xs:schema/xs:element");
-			Map<String,String> namespaces = new HashMap<String,String>();
-			namespaces.put("xs",namespace);
+			Map<String, String> namespaces = new HashMap<String, String>();
+			namespaces.put("xs", namespace);
 			xpath.setNamespaceURIs(namespaces);
 			List<Node> nodes = xpath.selectNodes(document);
+			int id = 1;
+			List<JSONObject> jsonList = new ArrayList<JSONObject>();
 			for (Node node : nodes) {
-	            System.out.println("\nCurrent Element :"
-	               + node.getName());
+				JSONObject jsonObject = new JSONObject();
+				jsonObject.put("id", id);
+				jsonObject.put("name", node.valueOf("@name"));
+				id++;
+				System.out.println("\nCurrent Element :" + node.valueOf("@name"));
+				jsonList.add(jsonObject);
 			}
-			
-			
-		} catch (DocumentException e) {
+			JSONObject treeJson = new JSONObject();
+			treeJson.put("tree", jsonList);
+			return treeJson;
+		} catch (Exception e) {
 			e.printStackTrace();
+			return null;
 		}
 	}
 
