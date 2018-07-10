f3284d358f34be230ba73fb1a1161dd8fd5dd71e
omjigupta <omji.mcs.du.2014@gmail.com>
2018-07-10 16:41:07 +0530

    has children property added

--- src/main/java/com/p3/archon/xsd_generation/beans/Children.java
+++ src/main/java/com/p3/archon/xsd_generation/beans/Children.java
@@ -20,6 +20,7 @@ public class Children {
   Long id;
   String filename;
   List<Children> children = new ArrayList<Children>();
+  String hasChildren;
 
   @Override
   public String toString() {
@@ -32,6 +33,7 @@ public class Children {
             ", id=" + id +
             ", filename='" + filename + '\'' +
             ", children=" + children +
+            ", hasChildren='" + hasChildren + '\'' +
             '}';
   }
 }
